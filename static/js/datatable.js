5ba8e9a4d7f9bc39ddb4ef16c0aaa65757aa3cbc
Hugo Ruscitti <hugoruscitti@gmail.com>
2012-07-26 17:49:13 -0300

    Mejorando estilo de la tabla.

--- static/js/datatable.js
+++ static/js/datatable.js
@@ -1,12 +1,19 @@
 $(document).ready(function() {
-        $('#retiros').dataTable({
-            "bLengthChange": false,
-            //"bInfo": true,
-            //"bPaginate": true,
-            //"bSortable": false,
+  $.extend( $.fn.dataTableExt.oStdClasses, {
+    "sWrapper": "dataTables_wrapper form-inline"
+  });
 
-            //"bSort": true,
-            "bServerSide": true,
-            "sAjaxSource": "/obtener_retiros",
-    });
+  $('#retiros').dataTable({
+    "bLengthChange": false,
+    "bInfo": true,
+    "bPaginate": true,
+    "bSortable": false,
+    "bSort": true,
+   
+    "bServerSide": true,
+    "sAjaxSource": "/obtener_retiros",
+    "sDom": "<'row'<'span6'l><'span6'f>r>t<'row'<'span6'i><'span6'p>>",
+  });
+
+
 });
