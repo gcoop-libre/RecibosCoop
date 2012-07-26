eeb485c40c926c24e4372718578153ad4f12ae43
Hugo Ruscitti <hugoruscitti@gmail.com>
2012-07-26 20:43:35 -0300

    Agregando campo fecha y mejorando el validador del importador.

--- forms.py
+++ forms.py
@@ -1,4 +1,5 @@
-from flaskext.wtf import Form, TextAreaField, Required
+from flaskext.wtf import Form, TextAreaField, Required, DateField
 
 class ImportarForm(Form):
     montos = TextAreaField('Montos a retirar', validators=[Required()])
+    fecha = DateField("Fecha", validators=[Required()])
