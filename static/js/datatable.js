9a1ad1bb0378b997558f9bb816008f79fd116aab
Joaquín Sorianello <joac@gcoop.coop>
2012-08-09 18:11:52 -0300

    Fix en javascript para seleccion de recibos a generar

--- static/js/datatable.js
+++ static/js/datatable.js
@@ -3,6 +3,20 @@ $(document).ready(function() {
     "sWrapper": "dataTables_wrapper form-inline"
   });
 
+  function cambio_checkbox_recibo(){
+    if (!this.checked){
+      $('#seleccionar_todos').attr('checked', false);
+    }
+  }
+
+  function cambio_checkbox(){
+    if (this.checked){
+      $('.selector_recibo').attr('checked', true);
+    } else { 
+      $('.selector_recibo').attr('checked', false);
+    }
+  }
+
   var table = $('#retiros').dataTable({
     bLengthChange: false,
     bInfo: true,
@@ -13,17 +27,24 @@ $(document).ready(function() {
 
     /* Traducciones */
     oLanguage: {
-			sSearch: "Buscar:",
-			sZeroRecords: "No hay nada que mostrar",
-			sInfo: "Mostrando de _START_ a _END_ (hay _MAX_ registros en total)",
-			sInfoEmpty: "No hay registros que mostrar",
-			sInfoFiltered: "(filtrado de _MAX_ registros)",
-		},
+      sSearch: "Buscar:",
+    sZeroRecords: "No hay nada que mostrar",
+    sInfo: "Mostrando de _START_ a _END_ (hay _MAX_ registros en total)",
+    sInfoEmpty: "No hay registros que mostrar",
+    sInfoFiltered: "(filtrado de _MAX_ registros)",
+    },
 
     "bServerSide": true,
     "sAjaxSource": "/obtener_retiros",
+    "fnDrawCallback": function (){
+      $('.selector_recibo').each(function(id){
+        $(this).change(cambio_checkbox_recibo);
+      });  
+    }
   });
 
+  $('#seleccionar_todos').change(cambio_checkbox);  
+  
   /* Obtiene los parametos de la URL */
   function get_url_param(name) {
     var results = new RegExp('[\\?&]' + name + '=([^&#]*)').exec(window.location.href);
@@ -40,6 +61,5 @@ $(document).ready(function() {
     $("input[aria-controls='retiros']").val(busqueda);
     table.fnFilter(busqueda);
   } 
-  
 
 });
