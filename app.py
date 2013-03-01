e73fa3f1771fbd5355d48424f4797b043bdca7eb
Hugo Ruscitti <hugoruscitti@gmail.com>
2013-03-01 15:12:05 -0300

    reparando el bug de paginacion fix #15

--- app.py
+++ app.py
@@ -121,7 +121,7 @@ def obtener_retiros():
     # Aplicando limites
     limite = int(request.args.get('iDisplayLength'))
     desde = int(request.args.get('iDisplayStart'))
-    retiros = retiros.order_by(('fecha', 'desc')).paginate(desde/limite, limite)
+    retiros = retiros.order_by(('fecha', 'desc')).paginate((desde/limite) + 1, limite)
 
     datos = [convertir_en_formato_de_tabla(d) for d in retiros]
     total = retiros.count()
