5207dea441055be5a9bd4f6bd78f216628112a7a
hugoruscitti <hugoruscitti@gmail.com>
2013-07-03 12:37:28 -0300

    arreglos menores en el deploy inicial y migracion de datos.

--- migrar.py
+++ migrar.py
@@ -1,9 +1,12 @@
 import sqlite3
 
-print "Agregando la columna 'concepto' a la tabla retiro."
 con = sqlite3.connect('recibos.db')
 cur = con.cursor()
-cur.execute('alter table retiro add column concepto char(255)')
+try:
+    cur.execute('alter table retiro add column concepto char(255)')
+    print "Agregando la columna 'concepto' a la tabla retiro."
+except:
+    pass
 
 print "Definiendo un valor inicial para todos los conceptos."
 cur.execute("update retiro set concepto='anticipo de retornos a cuenta de excedentes'")
